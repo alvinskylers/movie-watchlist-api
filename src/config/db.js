ccb8fe2897d6d5815ee7282ce15c96b67248e3b4
alvinskylers <alvinskylers.dev@gmail.com>
2026-02-09 20:59:05 +0800

    fix: prisma pacakge issue fix: database connectivity add: data table models

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,9 @@
-import { PrismaClient } from "@prisma/client/";
+import { PrismaClient } from "@prisma/client";
 
-const prsima = new PrismaClient({
-    log: 
-        process.env.NODE_ENV === "development"
-            ? ["query", "error", "warn"]
-            : ["error"],
+const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === "development" 
+        ? ["query", "warn", "error"] 
+        : ["error"], 
 });
 
 const connectDatabase = async () => {
